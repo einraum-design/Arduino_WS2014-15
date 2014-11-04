9ee575b869a2a049da7009c3e4ff83b2588a2690
Manuel Michel <mail@manuelmichel.de>
2014-11-05 00:54:02 +0100

    sketch kommentiert

--- sketch_oct23a/sketch_oct23a.ino
+++ sketch_oct23a/sketch_oct23a.ino
@@ -1,24 +1,30 @@
-//int ledPin = 2;
 #define ledPin 2
 #define fadeLedPin 3
 
+// brightness value of led
 int brightness = 0;
+
+// delay in void loop
 int delayVal = 5;
+
+// count brightness up or down?
 boolean countUp = true;
 
-void setup() {
 
+void setup() {
+  // start serial connection with baudRate of 4800
   Serial.begin(4800);
-  // put your setup code here, to run once:
+  
+  // define ledPin as OUTPUT pin
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.println(brightness);
+  
+  // alter brightness value every frame
   if (countUp) {
     brightness += 10;
-
   } else {
     brightness -= 10;
   }
@@ -26,19 +32,24 @@ void loop() {
   //  werte auf bereich 0 - 255 begrenzen
   brightness = constrain(brightness, 0, 255);
 
+  // wenn ober oder untergrenze von brightness 
+  // über-/unterschritten wird, countUp invertieren
   if (brightness > 250  || brightness <  1) {
     countUp = !countUp;
   }
 
-  // delay umschalten
+  // delay ist unterschiedlich, je nachdem, ob die led ein- oder ausfaded
   if (countUp) {
     delayVal = 5;
   } else {
     delayVal = 50;
   }
 
+
+  // analogen Helligkeits Wert auf den fadeLedPin schreiben -> Licht faded an und aus
   analogWrite(fadeLedPin, brightness);
 
+  // digitalen Wert "An" oder "Aus" auf den ledPin schreiben
   // blink an
   digitalWrite(ledPin, HIGH);
   delay(delayVal);
